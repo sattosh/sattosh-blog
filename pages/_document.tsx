517925f54536095e577b3f2963bb39d473a89732
HiroshimaSatoshi <hirosh@wni.com>
2022-07-31 00:56:24 +0900

    webvitalの設定

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,21 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "d196fee4e915413a88677b8493ee6e51"}'
+          ></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
