731b35b437a81f2be665b174a33ec9ca49dc25c2
HiroshimaSatoshi <hirosh@wni.com>
2023-06-18 12:51:36 +0900

    chore: いらないファイルを削除

--- lib/api.ts
+++ lib/api.ts
@@ -12,8 +12,7 @@ export function getPostSlugs() {
 
 export function getPostBySlug(slug: string, fields: string[] = []) {
   const realSlug = slug.replace(/\.md$/, '');
-  console.log(postsDirectory);
-  console.log('test');
+
   const fullPath = resolve(postsDirectory, `${realSlug}.md`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
   const { data, content } = matter(fileContents);
@@ -22,16 +21,9 @@ export function getPostBySlug(slug: string, fields: string[] = []) {
 
   // Ensure only the minimal needed data is exposed
   fields.forEach((field) => {
-    if (field === 'slug') {
-      items[field] = realSlug;
-    }
-    if (field === 'content') {
-      items[field] = content;
-    }
-
-    if (typeof data[field] !== 'undefined') {
-      items[field] = data[field];
-    }
+    if (field === 'slug') items[field] = realSlug;
+    if (field === 'content') items[field] = content;
+    if (typeof data[field] !== 'undefined') items[field] = data[field];
   });
 
   return items as PostType;
